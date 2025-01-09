5b5a4c66bb3e6dcedd796bcc5a056aefb584da2d
JueKunn2004 <aditkunn02official@gmail.com.com>
2025-01-09 17:48:58 +0700

    first commit

---
+++ src/app/about/page.tsx
@@ -0,0 +1,10 @@
+export default function page() {
+    return (
+      <>
+      <div className="flex justify-center items-center h-screen">
+        
+        <h1>ini halaman about</h1>
+        </div>
+      </>
+    )
+  }
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import Introduction from "@/components/Introduction";
+import Tech from "@/components/Tech";
+
+export default function page() {
+  <>
+    <Introduction/>
+    <Tech/>
+  </>
+}
